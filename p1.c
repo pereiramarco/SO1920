d9924274145dc12c2a190cbe6755bfb47d6f55a2
MrMarisco <marcopereiravpa@gmail.com>
2020-06-02 14:00:09 +0100

    ajuda e algumas mudanças

---
+++ p1.c
@@ -0,0 +1,5 @@
+
+void main() {
+    getchar();
+    puts("ola");
+}
--- servidor.c
+++ servidor.c
@@ -4,35 +4,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <signal.h>
 
 #define MAX 256
 #define split " "
 
-int tempo_inatividade,tempo_execucao,pid[MAX],ntarefa;
 
-void tookTooLong(int s) {
-    int k=getpid();
-    printf("estou a procura de %d\n",k);
+/*dúvidas
+-> colocar int em string (posso usar sprintf?)(escrever int com write)
+*/
+
+//pid[MAX][MAX] matriz que contem no indice 0 o pid do pai, no indice 1 o numero da tarefa no indice 2 o numero de processos e nos restantes os pids dos filhos
+int tempo_inatividade,tempo_execucao,pid[MAX][MAX],ntarefa;
+
+int getPIDOfTask(int task) {
     for (int i=0;i<MAX;i++) {
-        if (pid[i]==k) {
-            printf("demorou demasiado id %d\n",k);
-            //TO-DO save result
-            kill(pid[i],SIGKILL);
-            pid[i]=0;
-            break;
+        if (pid[i][1]==task) {
+            int r=pid[i][0];
+            return r;
         }
     }
 }
 
-void addToPIDList(int pidd) {
-    int i;
-    for (i=0;i<MAX;i++) {
-        if (!pid[i])
-            pid[i]=pidd;
-        else break;
+int getIndexOfPID(int pidd) {
+    for (int i=0;i<MAX;i++) {
+        if (pid[i][0]==pidd)
+            return i;
     }
-    ntarefa++;
+    return -1;
+}
+
+void addProcess(int p,int pai) {
+    int i=getIndexOfPID(pai);
+    pid[i][pid[i][2]++]=p;
+}
+
+void tookTooLong(int s) {
+    int k=getpid();
+    int p=getIndexOfPID(k);
+    puts("someone took a lot");
+    if (p!=-1) {
+        printf("demorou demasiado id %d\n",k);
+        //TO-DO save result
+        kill(-pid[p][0],SIGKILL);
+        wait(NULL);
+        pid[p][1]=0;
+        pid[p][0]=0;
+        pid[p][2]=3;
+    }
+}
+
+int addToPIDList(int pidd) {
+    int i,r=-1;
+    for (i=0;i<MAX;i++) {
+        if (!pid[i][0]) {
+            pid[i][0]=pidd;
+            pid[i][1]=ntarefa;
+            ntarefa+=1;
+            r=pid[i][1];
+            break;
+        }
+    }
+    return r;
 }
 
 void doStuff(char * linha) {
@@ -64,13 +98,10 @@ void doStuff(char * linha) {
     }
     else if (!strcmp(splitedinput[0],"executar") || !strcmp(splitedinput[0],"-e")) {
         write(1,"Escolheu executar uma tarefa\n",29);
-        int c=0,p[2],k;
+        int c=0,p[2],k,n;
         if (tam>1) {
-            if (!fork()) {
+            if (!(k=fork())) {
                 signal(SIGALRM,tookTooLong);
-                k=getpid();
-                printf("iniciou em pid: %d\n",k);
-                addToPIDList(k);
                 for (i=0;splitedinput[1][i];i++) {
                     splitedinput[1][i]=splitedinput[1][i+1];
                 }
@@ -84,10 +115,9 @@ void doStuff(char * linha) {
                 }
                 pipe(p);
                 if (tempo_execucao>0) {
-                    puts("vou alarmar");
                     alarm(tempo_execucao);
                 }
-                if (!fork()) {
+                if (!(i=fork())) {
                     close(p[0]);
                     if (c-1>0) dup2(p[1],1);
                     close(p[1]);
@@ -95,12 +125,13 @@ void doStuff(char * linha) {
                     puts("exit");
                     exit(0);
                 }
+                if (c-1>0) waitpid(i,NULL,0);
                 for (i=1;i<c-1;i++) {
                     dup2(p[0],0);
                     close(p[1]);
                     close(p[0]);
                     pipe(p);
-                    if (!fork()) {
+                    if (!(k=fork())) {
                         dup2(p[1],1);
                         close(p[1]);
                         close(p[0]);
@@ -108,6 +139,7 @@ void doStuff(char * linha) {
                         puts("error");
                         exit(0);
                     }
+                    waitpid(k,NULL,0);
                 }
                 if (c-1>0) {
                     dup2(p[0],0);
@@ -117,23 +149,40 @@ void doStuff(char * linha) {
                     puts("falhou");
                     exit(0);
                 }
-                pause();
                 exit(0);
             }
+            i=addToPIDList(k);
+            printf("iniciou em pid: %d tarefa %d\n",k,i);
         }
         else r=1;
     }
     else if (!strcmp(splitedinput[0],"listar") || !strcmp(splitedinput[0],"-l")) {
         puts("listar");
+        write(1,"######TASKS######\n",18);
+        for (i=0;i<MAX;i++) {
+            if (pid[i][0]) {
+                //write(1,"Tarefa ",7); //how to put integer in string (can use sprintf??)
+                //write(1," em execução!\n",14);
+                printf("Tarefa %d em execução!\n",pid[i][1]);
+            }
+        }
     }
     else if (!strcmp(splitedinput[0],"terminar") || !strcmp(splitedinput[0],"-t")) {
         puts("termina");
+        if (tam>1) {
+            int p=getPIDOfTask(atoi(splitedinput[1]));
+            if (p) {
+                puts("vou matar");
+            }
+        }
+        else r=1;
     }
     else if (!strcmp(splitedinput[0],"historico") || !strcmp(splitedinput[0],"-r")) {
         puts("historico");
     }
     else if (!strcmp(splitedinput[0],"ajuda") || !strcmp(splitedinput[0],"-h")) {
         puts("ajuda");
+        printf("Executar task: -e ou executar\nMudar tempo inatividade: -i ou tempo-inatividade\nMudar tempo execução: -m ou tempo-execucao\nListar tarefas: -l ou listar\nTerminar tarefa: -t ou terminar\n");
     }
     else if (!strcmp(splitedinput[0],"output") || !strcmp(splitedinput[0],"-o")) {
         puts("consultar output");
@@ -152,7 +201,11 @@ void main() {
     tempo_inatividade=-1;
     tempo_execucao=-1;
     for (n=0;n<MAX;n++) {
-        pid[n]=0;
+        pid[n][0]=0;
+        pid[n][1]=0;
+        pid[n][2]=3;
+        for (int j=3;j<MAX;j++)
+            pid[n][j]=-1;
     }
     mkfifo("fifo",0666);
     index = open("index",O_RDWR | O_APPEND | O_CREAT, 0666);
@@ -162,6 +215,6 @@ void main() {
         while ((n=read(fifo,linha,MAX))) {
             doStuff(linha);
         }
-        puts("line read");
+        puts("client closed");
     }
 }
